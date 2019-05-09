2f730a2a73807cabe6ff2afc7e6a487b3362c3c5
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-05-09 11:11:42 +0200

    evg devel

--- cmds/evg.cmd
+++ cmds/evg.cmd
@@ -1,3 +1,5 @@
+# EVG FPGA img version: 207.8
+
 require mrfioc2, 2.2.0-rc5
 
 epicsEnvSet("IOC", "MTCA")
@@ -8,14 +10,17 @@ epicsEnvSet("HeartBeatEvtCODE", "122")
 epicsEnvSet("ESSEvtClockRate", "88.0525")
 
 mrmEvgSetupPCI($(DEV), "08:00.0")
-dbLoadRecords("evm-mtca-300-ess.db",  "SYS=$(IOC), D=$(DEV), EVG=$(DEV), FEVT=$(ESSEvtClockRate), FRF=$(ESSEvtClockRate), FDIV=1")
+dbLoadRecords("evm-mtca-300-ess.db", "SYS=$(IOC), D=$(DEV), EVG=$(DEV), FEVT=$(ESSEvtClockRate), FRF=$(ESSEvtClockRate), FDIV=1")
 
 # needed with software timestamp source w/o RT thread scheduling
 var evrMrmTimeNSOverflowThreshold 100000
 
 iocInit()
 
-dbpf "$(IOC)-$(DEV):1ppsInp-Sel" "Sys Clk"
+# FDIV=1 for the external RF does not work so it has to be done during the runtime
+dbpf $(IOC)-$(DEV):EvtClk-RFDiv-SP 1
+
+dbpf $(IOC)-$(DEV):1ppsInp-Sel "Sys Clk"
 
 # Set EVM as master
 dbpf $(IOC)-$(DEV):Enable-Sel "Ena Master"
@@ -29,17 +34,19 @@ dbpf $(IOC)-$(DEV):SoftSeq0-Enable-Cmd 1
 
 # Master Event Rate 14 Hz
 dbpf $(IOC)-$(DEV):Mxc0-Prescaler-SP 6289464
-#dbpf $(IOC)-$(DEV):TrigEvt0-EvtCode-SP $(MainEvtCODE)
-#dbpf $(IOC)-$(DEV):TrigEvt0-TrigSrc-Sel "Mxc0"
-system("/bin/bash ../iocsh/seq_14Hz.bash $(IOC) $(DEV)")
+# dbpf $(IOC)-$(DEV):TrigEvt0-EvtCode-SP $(MainEvtCODE)
+# dbpf $(IOC)-$(DEV):TrigEvt0-TrigSrc-Sel "Mxc0"
+# system("/bin/bash ../iocsh/seq_14Hz.bash $(IOC) $(DEV)")
+system("/bin/bash ../iocsh/configure_sequencer_prod.bash $(IOC) $(DEV)")
 
 # Heart Beat 1 Hz
 dbpf $(IOC)-$(DEV):Mxc7-Prescaler-SP 88052500
 dbpf $(IOC)-$(DEV):TrigEvt7-EvtCode-SP $(HeartBeatEvtCODE)
 dbpf $(IOC)-$(DEV):TrigEvt7-TrigSrc-Sel "Mxc7"
 
-# Is there something similar to sleep(5)????
+# Is there something similar to sleep(5)
 # dbpf $(IOC)-$(DEV):SyncTimestamp-Cmd 1
 
 # Dump the PV-s
+dbpr $(IOC)-$(DEV):FwVer-I > "$(IOC)-$(DEV)_img.ver"
 dbl > "$(IOC)-$(DEV)_PVs.list"
