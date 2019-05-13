0608f4b0c11e5867639881a6395d1211f17f917a
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-05-13 15:25:47 +0200

    evm tests

--- cmds/README.md
+++ cmds/README.md
@@ -1,2 +1,7 @@
-Basic Evaluation Directory for evr0 and evr1 ics lab test setup.
+# Evaluation/Test Directory
 
+# mTCA-mini mapping:
+
+AMC1 -> "0a:00.0"
+AMC5 -> "07:00.0"
+AMC6 -> "08:00.0"
---
+++ cmds/evm.cmd
@@ -0,0 +1,21 @@
+require mrfioc2, 2.2.0-rc5
+
+epicsEnvSet("IOC", "MTCA")
+epicsEnvSet("DEV", "EVM")
+
+epicsEnvSet("MainEvtCODE", "14")
+epicsEnvSet("HeartBeatEvtCODE", "122")
+epicsEnvSet("ESSEvtClockRate", "88.0525")
+
+mrmEvgSetupPCI($(DEV), "07:00.0")
+dbLoadRecords("evm-mtca-300-ess.db", "SYS=$(IOC), D=$(DEV), EVG=$(DEV), FEVT=$(ESSEvtClockRate), FRF=$(ESSEvtClockRate), RFDIV=1")
+
+# needed with software timestamp source w/o RT thread scheduling
+var evrMrmTimeNSOverflowThreshold 100000
+
+iocInit()
+
+# Dump the PV-s
+dbpr $(IOC)-$(DEV):FwVer-I > "$(IOC)-$(DEV)_PVs.list"
+dbl >> "$(IOC)-$(DEV)_PVs.list"
+
