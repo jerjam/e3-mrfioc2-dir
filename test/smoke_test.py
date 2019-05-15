7c351ab365a26e7135ffc8a9e9d469b125994a2c
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-05-15 15:05:38 +0200

    python3 venv set with conda and pyepics

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -2,9 +2,15 @@
 
 import sys
 
+# file environment test
 print(sys.version)
 
 if sys.version_info[0] < 3:
     raise Exception("ERROR: Python 3 required")
+
+# ---------------------------------------------
+# epics environment test
+import epics
+print(epics.ca.find_libca())
 
 print("This line will be printed.")
