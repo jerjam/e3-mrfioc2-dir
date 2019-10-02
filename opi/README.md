460f6f3926d7eb519936762015ff9c27278af4d1
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-10-02 16:00:47 +0200

    opi moved to another repository

--- opi/README.md
+++
@@ -1,7 +0,0 @@
- 	Currently, the following OPI files work as expected:
-  
-  * evr-pcie-300dc.opi
-  * evr-mtca-300.opi
-  * evr-mtca-300u.opi
-  * evg-cpci-230.opi
-  * ... opi
--- opi/evg-seq-clear.py
+++
@@ -1,8 +0,0 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil
-
-widget = display.getWidget("SeqSet")
-
-table = widget.getTable()
-
-while table.getRowCount()>0:
-	table.deleteRow(0)
